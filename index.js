e40b4c5cdf35e3d52639a2812b285f3467ac43e2
Evan Chernicky <echernicky@gmail.com>
2022-02-01 11:29:51 -0500

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("test")
+
+//Monday Work
+function mondayWork(args = "go to the office") {
+    return `This Monday, I will ${args}.`;
+}
+mondayWork("work from home")
+
+//wrapAdjective
+function wrapAdjective(wrapper = "*") {
+    return function(whatYouAre = "special") {
+        return `You are ${wrapper}${whatYouAre}${wrapper}!`
+    };
+};
+wrapAdjective("%")("cool");
